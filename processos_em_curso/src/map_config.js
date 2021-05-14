26cc280df6c54f901dbad78788df50ec6db3f49e
Rui Cavaco <rpcavaco@gmail.com>
2021-05-14 11:31:38 +0100

    widgetnames_hide_during_refresh
    widgetnames_hide_during_refresh com a legenda

--- processos_em_curso/src/map_config.js
+++ processos_em_curso/src/map_config.js
@@ -50,7 +50,7 @@ var MAPCFG = {
 			"elem_height": 27,
 			"max_cols": 4
 		},
-		//"widgetnames_hide_during_refresh": ["legendctrl", "visctrl"],
+		"widgetnames_hide_during_refresh": ["legendctrl"],
 		"toollayeractions": {
 			"picker": {			
 				"mousemove": {
